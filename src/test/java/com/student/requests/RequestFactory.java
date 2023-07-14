0634b52e10949392744959a8603556c1d4affe13
Carlos Bigurra <cabigurra@gmail.com>
2023-07-14 10:07:10 -0600

    test: add fake data to the tests with Faker

--- src/test/java/com/student/requests/RequestFactory.java
+++ src/test/java/com/student/requests/RequestFactory.java
@@ -1,9 +1,12 @@
 package com.student.requests;
 
+import com.student.pojo.StudentClass;
 import com.student.tests.TestBase;
 import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+
+import java.util.List;
 
 public class RequestFactory extends TestBase {
 
@@ -13,6 +16,18 @@ public class RequestFactory extends TestBase {
         Response response = restClient.doGetRequest("");
 
         return response;
+    }
+
+    public Response createNewStudent(String url, String firstName, String lastName, String email,
+                                     String programme, List<String> courses) {
+        StudentClass body = new StudentClass();
+        body.setFirstName(firstName);
+        body.setLastName(lastName);
+        body.setEmail(email);
+        body.setProgramme(programme);
+        body.setCourses(courses);
+
+        return restClient.doPostRequest(url, body);
     }
 
 }
--- src/test/java/com/student/tests/CrudTest.java
+++ src/test/java/com/student/tests/CrudTest.java
@@ -1,11 +1,15 @@
 package com.student.tests;
 
+import com.github.javafaker.Faker;
 import com.student.requests.RequestFactory;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Story("This is a CRUD testing story")
 public class CrudTest extends TestBase {
@@ -23,5 +27,26 @@ public class CrudTest extends TestBase {
                 .body()
                 .statusCode(200);
 
+    }
+
+    @Story("This is a CRUD testing story")
+    @DisplayName("Test to create & verify a new student")
+    @Feature("Test to create & verify a new student")
+    @Test
+    public void createNewStudent() {
+        Faker fakeData = new Faker();
+
+        String firstName = fakeData.name().firstName();
+        String lastName  = fakeData.name().lastName();
+        String email     = fakeData.internet().emailAddress();
+        String programme = "ComputerScience";
+
+        List<String> courses = new ArrayList<String>();
+        courses.add("C++");
+        courses.add("Java");
+
+        requests.createNewStudent("", firstName, lastName, email, programme, courses)
+                .then()
+                .statusCode(201);
     }
 }
